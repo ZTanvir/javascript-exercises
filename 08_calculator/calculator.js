a7d7d13253318a8fc274e46c0dc6a976a42bfb12
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-07-01 22:27:38 +0600

    Calculator exercise complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numbersArray) {
+	let sum = 0
+  for(let number of numbersArray){
+    sum += number
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(numbersArray) {
+  let result = 1
+  for(let number of numbersArray){
+    result *= number
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(base,power) {
+  let num = base ** power
+  return num
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  let sum = 1
+  if(number == 0){
+    return sum
+  }
+  if(number >0){
+    for(let i = 1 ;i <= number ; i++){
+      sum *= i
+    }
+    return sum
+  }
 	
 };
 
