fe97aec57c4b1b922b02df537b4b2c74e5fe2942
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-07-01 10:46:13 +0600

    Exercise sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    let sum = 0;
+    let message = "";
+    if (start < 0 || typeof end !== "number") {
+        message += "ERROR";
+        return message;
+    } else if (start > end) {
+        for (let i = end; i <= start; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
