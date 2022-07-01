3c8fafb4e67bce95968694e5beec3a024f54fffd
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-07-01 21:45:30 +0600

    tempConversion exercise complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (far) {
+  let celsius = (far - 32) * (5 / 9);
+  celsius = Number(celsius.toFixed(1));
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function (cel) {
+  let fahrenheit = cel * 1.8 + 32;
+  fahrenheit = Number(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
+
 
 // Do not edit below this line
 module.exports = {
