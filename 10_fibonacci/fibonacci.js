75a1f87b7d313bd0a19d1ec4a516c6bbb2501055
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-07-02 13:26:29 +0600

    Fibonacci exercise done 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (nthMemberFibonacci) {
+    if (nthMemberFibonacci < 0) {
+        return "OOPS";
+    } else {
+        let nthInFibo = Number(nthMemberFibonacci);
+        let f0 = 0;
+        let f1 = 1;
+        let fibo = [f0, f1];
 
+        for (let i = 2; i <= nthInFibo; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        return fibo[nthInFibo];
+    }
 };
 
 // Do not edit below this line
