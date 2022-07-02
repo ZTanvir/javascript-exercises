2a585184376d82039d7a3c103b225ff5ff8095ca
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-07-02 12:27:11 +0600

    Plaindromes exercise done 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,41 @@
-const palindromes = function () {
+const palindromes = function (fullSentence) {
+    let sentence = "";
+    // Remove all !,.,, from the word/sentence
+    for (let i = 0; i < fullSentence.length; i++) {
+        if (
+            fullSentence[i] != "!" &&
+            fullSentence[i] != "." &&
+            fullSentence[i] != ","
+        ) {
+            sentence += fullSentence[i];
+        }
+    }
+    // Convert to lowercase so the user can input uppercase
+    sentence = sentence.toLowerCase();
+    // Reverse the word
+    let reverse = "";
+    for (let i = sentence.length - 1; i >= 0; i--) {
+        reverse += sentence[i];
+    }
+    // Remove space between sentence
+    let removeWhiteSpace = function (checkSentence) {
+        let allWord = checkSentence;
+        allWord = allWord.split(" ");
+        let nospaceReverseWord = "";
+        for (let letter of allWord) {
+            nospaceReverseWord += letter;
+        }
+        return nospaceReverseWord;
+    };
+    let reverseCompareWord = removeWhiteSpace(reverse);
+    let mainWord = removeWhiteSpace(sentence);
 
+    // Compare both orginal word vs reverse word
+    if (reverseCompareWord === mainWord) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
