2e05ec238cd8d790404f190dbf3eab0fd3c8f153
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-07-03 11:57:21 +0600

    findTheOldest exercise complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    oldestPerson = "";
+    age = 0;
 
+    for (let i = 0; i < people.length; i++) {
+        personAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        if (typeof personAge != "Number") {
+            let timeNow = new Date();
+            let timeYear = timeNow.getFullYear();
+            personAge = timeYear - people[i].yearOfBirth;
+        }
+        if (personAge > age) {
+            oldestPerson = people[i].name;
+            age = personAge;
+        }
+    }
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].name != oldestPerson) {
+            people.splice(i, 1);
+        }
+    }
+    return people[0];
 };
 
 // Do not edit below this line
